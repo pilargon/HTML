236f6b799fb3882bda5bad7e2e7cd502ba06ee05
Pilar G <pilargonzalezcla@gmail.com>
2018-11-06 15:52:57 +0100

    j

--- Solution1/app.js
+++ Solution1/app.js
@@ -12,7 +12,7 @@ function cambiarFecha() {
     //    + year + "-" + month + "-" + day); //1a parte
     xhr.open("GET", "https://api.nasa.gov/planetary/apod?api_key=2oqMwUicGmGKIUdJQv6LDbQZGFcVVskfnBTKLZFU&date="
         + fechaCogida.value);//2a parte
-    
+
     xhr.onreadystatechange = function () {
 
         console.log(xhr.readyState);
@@ -20,6 +20,9 @@ function cambiarFecha() {
 
             let responseObject = JSON.parse(xhr.response);
             showData(responseObject.date, responseObject.title, responseObject.explanation, responseObject.url);
+
+            let date = JSON.parse(xhr.response); //4a parte  TODO:
+            escribirFecha(date);    //4a parte
 
         } else if (xhr.readyState === 4 & xhr.status === 400) {
             document.getElementById("info").innerHTML = "Fecha incorrecta";
@@ -29,10 +32,33 @@ function cambiarFecha() {
     xhr.send();
 };
 
-function showData(date,title,explanation,image_url) {
+function showData(date, title, explanation, image_url) {
     let result = "<p>" + date + "</p>";
     result += "<p>" + title + "</p>";
     result += "<p>" + explanation + "</p>";
     result += "<img src='" + image_url + "'/>";
     document.getElementById("info").innerHTML = result;
 }
+
+//4 - En vez de mostrar la información de un día, hacer que en un mismo string, 
+//se sumen las descripciones desde la fecha indicada por el usuario hasta 14 días antes.
+
+
+function escribirFecha(date) {
+    let index = 0;
+    let xhrDate = new XMLHttpRequest();
+    xhrDate.open("GET", date[index]);
+    xhrDate.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(JSON.parse(this.response).name);
+            index++;
+            if (index < 14) {
+                xhrDate.open("GET", date[index]);
+                xhrDate.send();
+            }
+        }
+    };
+    xhrDate.send();
+
+}
+
